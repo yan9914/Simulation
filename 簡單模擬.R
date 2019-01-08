289148eec950bd3be9d99cca5f93d208e518d97f
yan9914 <31344747+yan9914@users.noreply.github.com>
2019-01-08 11:56:47 +0800

    Add files via upload

---
+++ 簡單模擬.R
@@ -0,0 +1,25 @@
+## 1. 一副撲克牌52張抽5張, 一張Ace都沒有的機率
+
+# 模擬
+v <- NULL
+for(i in 1 : 1e5){
+  v[i] <- all(sample(52, 5) > 4)
+}
+mean(v)
+# 實際
+choose(4,0)*choose(48,5)/choose(52,5)
+
+## 2. 產生10個Uniform(5,7)的隨機值
+2*runif(10)+5
+
+## 3. 獨立地擲10次公正骰子的結果, i.e. Discrete Uniform(1,2,...,6)
+ceiling(6*runif(10))
+sample.int(6, 10, replace = TRUE)
+
+## 4. 產生10個Discrete Uniform(5,6,...15)的隨機值
+ceiling(11*runif(10)) + 4
+sample(5:15, 10, replace = TRUE)
+
+## 5. 產生10個Bin(5, 0.3)的隨機值
+replicate(10, sum(runif(5) < 0.3))
+rbinom(10, 5, 0.3)
